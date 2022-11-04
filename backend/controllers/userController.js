2d481b1b0df60cf59d14887fac84ebbdc8c824e2
atabek-bakh <coderonline@icloud.com>
2022-11-04 23:54:25 +0100

    json web token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 const asyncHandler = require('express-async-handler')
-// const generateToken = require('../utils/generateToken.js')
+const generateToken = require('../utils/generateToken.js')
 const User = require('../models/userModel.js')
 
 const authUser = asyncHandler(async (req, res) => {
@@ -15,7 +15,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      // token: generateToken(user._id),
+      token: generateToken(user._id),
     })
   } else {
     res.status(401)
