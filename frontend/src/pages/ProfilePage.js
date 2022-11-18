830a0c628f159b9297997f8cc6beb500eacbaa80
tonyclem <tonyclem001@gmail.com>
2022-11-18 15:11:15 +0100

    added profile styles

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import "../styles/profilePage.css";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>
-}
+  return (
+    <div>
+      <h1>Welcome Profile Page</h1>
+    </div>
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
