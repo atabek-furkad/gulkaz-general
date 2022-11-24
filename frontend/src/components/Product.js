4f672c1a5934bc894716c4d776c98d7ed6a68953
tonyclem <tonyclem001@gmail.com>
2022-11-24 22:01:00 +0100

    added fixed error on upload multiple images

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,9 +1,9 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import UserContext from '../context/UserContext'
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import UserContext from '../context/UserContext';
 
 const Product = ({ product }) => {
-  const { state } = useContext(UserContext)
+  const { state } = useContext(UserContext);
 
   const handleFormSubmit = async (id) => {
     const config = {
@@ -11,30 +11,33 @@ const Product = ({ product }) => {
       headers: {
         Authorization: `Bearer ${state.userInfo.token}`,
       },
-    }
-    await fetch(`/api/products/${id}`, config)
-  }
+    };
+    await fetch(`/api/products/${id}`, config);
+  };
 
-  // const imagePath = product.image.slice(16)
+  const imagePath = product.image.slice(16);
 
   return (
     <div>
       <form
         onSubmit={(event) => {
           // event.preventDefault()
-          handleFormSubmit(product._id)
+          handleFormSubmit(product._id);
         }}
       >
         <Link to={`/products/${product._id}`}>{product.name}</Link>
-        {product.image.forEach((i) => {
-          ;<img src={i} alt="product image" height="100" />
+        {/* {imagePath.map((images, index) => (
+          <img src={images} key={index} alt='' />
+        ))} */}
+        {imagePath.map((images, index) => {
+          return <img src={images} alt='' key={index} />;
         })}
 
         <Link to={`/profile/edit-product/${product._id}`}>Edit</Link>
         <button>Delete</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
--- frontend/src/pages/NewProductPage.js
+++ frontend/src/pages/NewProductPage.js
@@ -1,19 +1,19 @@
-import BackButton from '../components/BackButton'
-import useFetch from '../customHooks/useFetch'
-import { useState, useEffect, useContext } from 'react'
-import UserContext from '../context/UserContext'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import BackButton from '../components/BackButton';
+import useFetch from '../customHooks/useFetch';
+import { useState, useEffect, useContext } from 'react';
+import UserContext from '../context/UserContext';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const NewProductPage = () => {
   const { product, setProduct, loading, error, fetchData } = useFetch(
     '/api/products',
-    'POST',
-  )
+    'POST'
+  );
 
   useFetch(() => {
-    console.log('product', product)
-  }, [product])
+    console.log('product', product);
+  }, [product]);
   // const { state } = useContext(UserContext)
   // const navigate = useNavigate()
 
@@ -28,9 +28,9 @@ const NewProductPage = () => {
 
   // const [error, setError] = useState(false)
   // const [loading, setLoading] = useState(false)
-  const [uploading, setUploading] = useState(false)
+  const [uploading, setUploading] = useState(false);
 
-  const [imagePath, setImagePath] = useState([])
+  const [imagePath, setImagePath] = useState([]);
 
   // const config = {
   //   method: 'POST',
@@ -49,21 +49,19 @@ const NewProductPage = () => {
   // }
 
   useEffect(() => {
-    console.log('product', product)
-    console.log('product.topProduct', product.topProduct)
-  }, [product])
+    console.log('product', product);
+    console.log('product.topProduct', product.topProduct);
+  }, [product]);
 
   const uploadFileHandler = async (e) => {
     // const file = e.target.files[0]
 
     // object of objects turning into array of objects
-    const arrayOfFiles = Object.values(e.target.files)
+    const arrayOfFiles = Object.values(e.target.files);
     // const myObjects = e.target.files
-    const galleryData = new FormData()
+    const galleryData = new FormData();
 
-    for (let i = 0; i < arrayOfFiles.length; i++) {
-      galleryData.append('image', arrayOfFiles[i])
-    }
+    arrayOfFiles.forEach((index) => galleryData.append('image', index));
 
     // console.log('galleryData', galleryData)
 
@@ -76,7 +74,7 @@ const NewProductPage = () => {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
-    }
+    };
 
     // const mapped = arrayOfFiles.map((file) => {
     //   const formData = new FormData()
@@ -90,33 +88,33 @@ const NewProductPage = () => {
 
     // console.log("formData", formData)
 
-    const { data } = await axios.post('/api/upload', await galleryData, config)
+    const { data } = await axios.post('/api/upload', await galleryData, config);
 
-    console.log('image path data', data)
+    console.log('image path data', data);
 
     const imagePaths = await data.map((image) => {
-      console.log('image paths', image.path)
-      return image.path
-    })
+      console.log('image paths', image.path);
+      return image.path;
+    });
 
-    console.log('imagePaths', imagePaths)
+    console.log('imagePaths', imagePaths);
 
     setProduct({
       ...product,
       image: imagePaths,
-    })
+    });
 
     const links = data.map((file) => {
-      return file.path.slice(15)
-    })
+      return file.path.slice(15);
+    });
 
     // const path = await data.slice(16)
 
     // console.log('data checking path', path)
 
-    setImagePath(links)
+    setImagePath(links);
 
-    console.log(links)
+    console.log(links);
     // })
 
     // console.log('arrayOfFiles', arrayOfFiles)
@@ -157,7 +155,7 @@ const NewProductPage = () => {
     //   console.error(error)
     //   setUploading(false)
     // }
-  }
+  };
 
   // const handleFormSubmit = async () => {
   //   setLoading(true)
@@ -182,78 +180,78 @@ const NewProductPage = () => {
     setProduct({
       ...product,
       [event.target.name]: event.target.value,
-    })
-  }
+    });
+  };
 
   return (
-    <div className="NewProductPage">
+    <div className='NewProductPage'>
       <BackButton />
       <h1>New Product Page</h1>
       <form
         onSubmit={(e) => {
-          e.preventDefault()
-          fetchData()
+          e.preventDefault();
+          fetchData();
         }}
       >
         {loading && <h2>Processing...</h2>}
         {error && <h2>{error}</h2>}
         {error && <h2>Fill all the inputs</h2>}
 
-        <div className="input-container">
-          <label htmlFor="name">Name</label>
+        <div className='input-container'>
+          <label htmlFor='name'>Name</label>
           <input
-            id="name"
-            name="name"
-            type="text"
+            id='name'
+            name='name'
+            type='text'
             value={product.name}
             onChange={handleInputChange}
           />
         </div>
-        <div className="input-container">
-          <label htmlFor="description">Description</label>
+        <div className='input-container'>
+          <label htmlFor='description'>Description</label>
           <input
-            id="description"
-            name="description"
-            type="text"
+            id='description'
+            name='description'
+            type='text'
             value={product.description}
             onChange={handleInputChange}
           />
         </div>
-        <div className="input-container">
-          <label htmlFor="countInStock">CountInStock</label>
+        <div className='input-container'>
+          <label htmlFor='countInStock'>CountInStock</label>
           <input
-            id="countInStock"
-            name="countInStock"
-            type="number"
+            id='countInStock'
+            name='countInStock'
+            type='number'
             value={product.countInStock}
             onChange={handleInputChange}
           />
         </div>
-        <div className="input-container">
-          <label htmlFor="category">Category</label>
+        <div className='input-container'>
+          <label htmlFor='category'>Category</label>
           <input
-            id="category"
-            name="category"
-            type="text"
+            id='category'
+            name='category'
+            type='text'
             value={product.category}
             onChange={handleInputChange}
           />
         </div>
-        <div className="input-container">
-          <label htmlFor="price">Price</label>
+        <div className='input-container'>
+          <label htmlFor='price'>Price</label>
           <input
-            id="price"
-            name="price"
-            type="number"
+            id='price'
+            name='price'
+            type='number'
             value={product.price}
             onChange={handleInputChange}
           />
         </div>
-        <div className="input-container">
-          <label htmlFor="topProduct">Top product</label>
+        <div className='input-container'>
+          <label htmlFor='topProduct'>Top product</label>
           <select
-            id="topProduct"
-            name="topProduct"
+            id='topProduct'
+            name='topProduct'
             value={
               product.topProduct
                 ? product.topProduct
@@ -266,26 +264,26 @@ const NewProductPage = () => {
           </select>
         </div>
 
-        <div className="input-container">
+        <div className='input-container'>
           {uploading && <h2>Uploading...</h2>}
-          <label htmlFor="image-upload">Choose a picture:</label>
+          <label htmlFor='image-upload'>Choose a picture:</label>
           <input
-            type="file"
-            id="image-upload"
-            name="image-upload"
-            multiple="multiple"
-            accept="image/png, image/jpeg, image/png"
+            type='file'
+            id='image-upload'
+            name='image-upload'
+            multiple='multiple'
+            accept='image/png, image/jpeg, image/png'
             onChange={uploadFileHandler}
           />
         </div>
         {imagePath.map((path, index) => {
-          console.log('path', path)
-          return <img width="100" key={index} src={path} />
+          console.log('path', path);
+          return <img width='100' key={index} src={path} />;
         })}
-        <button type="Submit">Create</button>
+        <button type='Submit'>Create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default NewProductPage
+export default NewProductPage;
