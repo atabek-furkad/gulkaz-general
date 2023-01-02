850cdaf3d8d0de6ffc69763a810af30a5a18de37
tonyclem <tonyclem001@gmail.com>
2023-01-02 22:05:16 +0100

    modified side bar

--- frontend/src/components/dashboardSideBar/SideBar.jsx
+++ frontend/src/components/dashboardSideBar/SideBar.jsx
@@ -8,22 +8,39 @@ import { BiLogOut } from 'react-icons/bi';
 
 import './sidebar.scss';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const SideBar = () => {
+  const [isActive, setActive] = useState(false);
+
+  const toggleIsActive = () => {
+    setActive(!isActive);
+  };
+
   return (
     <aside className='aside'>
       <div className='center'>
         <ul>
-          <li>
-            <BsShop className='icon' />
-            <span>DashBoard</span>
-          </li>
+          <Link
+            to='/profile/dashboard'
+            style={{ textDecoration: 'none', color: 'black' }}
+          >
+            <li className={isActive ? 'active' : null} onClick={toggleIsActive}>
+              <BsShop className='icon' />
+              <span>DashBoard</span>
+            </li>
+          </Link>
           <div className='product'>
             <p>Product</p>
-            <li>
-              <BsFolder2Open className='icon' />
-              <span>Show All Product</span>
-            </li>
+            <Link
+              to='/profile'
+              style={{ textDecoration: 'none', color: 'black' }}
+            >
+              <li>
+                <BsFolder2Open className='icon' />
+                <span>Show All Product</span>
+              </li>
+            </Link>
             <Link
               to='/profile/new-product'
               style={{ textDecoration: 'none', color: 'black' }}
@@ -33,10 +50,15 @@ const SideBar = () => {
                 <span>Add Product</span>
               </li>
             </Link>
-            <li>
-              <BsCloudArrowUpFill className='icon' />
-              <span>Update Product</span>
-            </li>
+            <Link
+              to='/profile'
+              style={{ textDecoration: 'none', color: 'black' }}
+            >
+              <li>
+                <BsCloudArrowUpFill className='icon' />
+                <span>Update Product</span>
+              </li>
+            </Link>
           </div>
 
           <div className='user'>
